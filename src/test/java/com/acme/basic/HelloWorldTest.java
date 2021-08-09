87bdf8bc9272942f7edee407e7cfb6cae9bf3d79
pooja03ydv <36677428+pooja03ydv@users.noreply.github.com>
2021-08-09 22:30:08 +0530

    Update HelloWorldTest.java

--- src/test/java/com/acme/basic/HelloWorldTest.java
+++ src/test/java/com/acme/basic/HelloWorldTest.java
@@ -1,11 +1,18 @@
 package com.acme.basic;
 
-import org.junit.Test;
+public class HelloWorld
+{
 
-public class HelloWorldTest {
+    private final String message = "Hello World!";
 
-  @Test
-  public void sayHello() {
-    new HelloWorld().sayHello();
-  }
+    public HelloWorld() {}
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
 }
